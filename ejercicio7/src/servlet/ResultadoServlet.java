93cacbe1c0c828f447a4f08e804d2a5686bc7821
Rodolfo Campos <camposer@gmail.com>
2015-03-04 12:27:21 +0100

    Solución del ejercicio 7

---
+++ ejercicio7/src/servlet/ResultadoServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Resultado")
+public class ResultadoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		String sedad = request.getParameter("edad");
+		Integer edad = Integer.parseInt(sedad); // Esto puede fallar!!
+		String btnAgregar = request.getParameter("btnAgregar");
+		
+		response.setCharacterEncoding("utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("</head>");
+		pw.println("<body>");
+		
+		if (btnAgregar != null)
+			pw.println("Agregando...<br>");
+		else
+			pw.println("Modificando...<br>");
+		
+		pw.println("Nombre: " + nombre + "<br>");
+		pw.println("Apellido: " + apellido + "<br>");
+		pw.println("Edad: " + edad + "<br>");
+			
+		pw.println("</body>");
+		pw.println("</html>");
+	}
+
+}
